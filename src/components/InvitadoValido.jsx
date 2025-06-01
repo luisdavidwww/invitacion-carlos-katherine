e73a4d9f54408aca7293eef77beb30bd28c45821
Luis Torrealba <luisdavid25torrealba@gmail.com>
2025-06-01 18:33:29 -0400

    datos cool!

--- src/components/InvitadoValido.jsx
+++ src/components/InvitadoValido.jsx
@@ -3,8 +3,20 @@ import React from 'react';
 const InvitadoValido = ({ Invitado }) => {
   return (
     <div className='header-container-two'>
-      <h2>{Invitado.nombres}</h2>
-      <p>Número de invitados: {Invitado.numInvitados}</p>
+        
+            <h2 className='title-Invitado'>{Invitado.nombres}</h2>
+            <p className='text-Invitado'>¡ESPERAMOS QUE PUEDAN COMPARTIR ESTA FIESTA JUNTO A NOSOTROS!</p>
+
+            <p className='text-Invitado' style={{marginTop:'1.5rem'}}>Número de invitados: 
+            </p>
+            <p className='title-numInvitado'> {Invitado.numInvitados}
+            </p>
+
+            <div className='containerbtn-two'>
+                <button className='btn-outline-two' type='button' >ABRIR INVITACIÓN</button>
+            </div>
+
+      
     </div>
   );
 };
